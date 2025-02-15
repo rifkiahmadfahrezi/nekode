a04fa8eddb7aba58185af119f99ae88542daff24
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-02-15 10:18:06 +0700

    Add language configuration for multilingual support

--- config/site.ts
+++ config/site.ts
@@ -2,3 +2,16 @@ export const siteConfig = {
    name: "Nekode",
    description: "Nekode is a free and open source platform to learn web development."
 }
+
+export const languages = [
+   {
+     name: 'English',
+     locale: 'en',
+   },
+   {
+     name: 'Indonesian',
+     locale: 'id',
+   },
+ ]
+
+export type Language = typeof languages[0]
