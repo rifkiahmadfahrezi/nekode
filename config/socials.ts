b928796ea9d1a8b5569a29c3bee6b69a15e6c187
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-02-15 18:46:47 +0700

    Add social links configuration with icons for Nekode and personal profiles

---
+++ config/socials.ts
@@ -0,0 +1,50 @@
+import { 
+   RiGithubFill,
+   RiInstagramLine,
+   RiTiktokFill,
+   RiLinkedinBoxFill,
+   RiGlobalLine
+} from '@remixicon/react'
+
+
+export const socials = {
+   nekode: [
+      {
+         label: 'Github',
+         link: 'https://github.com/nekode-id',
+         icon: RiGithubFill
+      },
+      {
+         label: 'Instagram',
+         link: 'https://www.instagram.com/nekode.id/',
+         icon: RiInstagramLine
+      },
+      {
+         label: 'Tiktok',
+         link: 'https://www.tiktok.com/@nekode.id',
+         icon: RiTiktokFill
+      },
+   ],
+   personal: [
+      {
+         label: 'Website',
+         link: 'https://rifki.pages.dev/',
+         icon: RiGlobalLine
+      },
+      {
+         label: 'Github',
+         link: 'https://github.com/rifkiahmadfahrezi',
+         icon: RiGithubFill
+      },
+      {
+         label: 'Instagram',
+         link: 'https://www.instagram.com/rifkii_30',
+         icon: RiInstagramLine
+      },
+      {
+         label: 'LinkedIn',
+         link: 'https://www.linkedin.com/in/rifki-ahmad-fahrezi/',
+         icon: RiLinkedinBoxFill
+      },
+   ]
+}
