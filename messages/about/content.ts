ca38410c0325f280f86a021fde17a530e8f87a13
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-02-15 14:31:53 +0700

    Add about content in English and Indonesian for Nekode website

---
+++ messages/about/content.ts
@@ -0,0 +1,19 @@
+export const aboutContent = [
+   {
+      lang: 'en',
+      content: `
+      <h1 class="~text-2xl/4xl font-bold">About Nekode</h1><p>Hi 👋 !, my name is <a target="_blank" href="https://rifkiaf.my.id">Rifki Ahmad Fahrezi</a>, a  22 y.o junior frontend web developer based in Bandung, Indonesia. Currently, I work as a freelancer in the frontend field. I’m offering frontend development services using React or Next.js, such as slicing UIs and API integration. Additionally, I can do UI/UX design using Figma. <a target="_blank" href="https://rifkiaf.my.id/projects">Check out my portfolio</a>.</p><p>I’m a self-taught developer, so I mostly write summaries of what I’ve learned on my Notion. I think sharing what I’ve learned with others makes my learning journey more meaningful than just keeping my summaries to myself. That’s why I built Nekode—a place where I share my learning journey and summaries, turning them into structured, free courses.</p><blockquote>Fun fact: The name ‘Nekode’ comes from two things I love: the Japanese word ‘neko (猫),’ meaning cat, combined with the word “code.”</blockquote><p>Hopefully, you can learn something or find useful content on this website 😊.</p><p>— Rifki</p>
+      `
+   },
+   {
+      lang: 'id',
+      content: `
+      <h1 class="~text-2xl/4xl font-bold">Tentang Nekode</h1>
+         <p>Hai 👋 !, nama saya <a target="_blank" href="https://rifkiaf.my.id">Rifki Ahmad Fahrezi</a>, seorang junior frontend web developer berusia 22 tahun yang berbasis di Bandung, Indonesia. Saat ini, saya bekerja sebagai freelancer di bidang frontend. Saya menawarkan layanan pengembangan frontend menggunakan React atau Next.js, seperti slicing UI dan integrasi API. Selain itu, saya juga bisa melakukan desain UI/UX menggunakan Figma. <a target="_blank" href="https://rifkiaf.my.id/projects">Lihat portofolio saya</a>.</p>
+         <p>Saya adalah seorang developer otodidak, jadi saya sering menulis ringkasan dari apa yang telah saya pelajari di Notion saya. Saya percaya bahwa berbagi ilmu yang saya pelajari dengan orang lain membuat perjalanan belajar saya lebih bermakna daripada hanya menyimpannya sendiri. Itulah sebabnya saya membangun Nekode—sebuah tempat di mana saya membagikan perjalanan belajar dan ringkasan saya, lalu mengubahnya menjadi kursus gratis yang terstruktur.</p>
+         <blockquote>Fun fact: Nama ‘Nekode’ berasal dari dua hal yang saya sukai, yaitu kata Jepang ‘neko (猫),’ yang berarti kucing, dikombinasikan dengan kata “code.”</blockquote>
+         <p>Semoga kamu bisa belajar sesuatu atau menemukan konten yang bermanfaat di website ini 😊.</p>
+         <p>— Rifki</p>
+      `
+   }
+]
