adfb7aebdfb212f1770f1920952a55a038514e90
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-03-01 19:43:41 +0700

    feat: integrate ImageZoom component for enhanced image handling in documentation

--- app/[lang]/docs/[[...slug]]/page.tsx
+++ app/[lang]/docs/[[...slug]]/page.tsx
@@ -7,6 +7,7 @@ import {
 } from 'fumadocs-ui/page';
 import { notFound } from 'next/navigation';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
+import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
 
 export default async function Page(props: {
   params: Promise<{ lang: string; slug?: string[] }>;
@@ -26,7 +27,10 @@ export default async function Page(props: {
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
-        <MDX components={{ ...defaultMdxComponents }} />
+        <MDX components={{ 
+          ...defaultMdxComponents,  
+          img: (props) => <ImageZoom {...(props)} />,  
+        }} />
       </DocsBody>
     </DocsPage>
   );
