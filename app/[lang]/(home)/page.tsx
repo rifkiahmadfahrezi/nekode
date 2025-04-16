8a0bb8816cc6b5f575f3418b7f4c603f8b855ab1
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-04-16 08:08:32 +0700

    feat: add giscus for easy comment interaction

--- app/[lang]/(home)/page.tsx
+++ app/[lang]/(home)/page.tsx
@@ -1,10 +1,11 @@
-import { Hero } from "@/components/page/home"
+import { Hero, Comment } from "@/components/page/home"
 
 export default function Home() {
     return (
     <section className="py-16 md:py-20">
         <div className="mx-auto max-w-screen-lg space-y-8 px-6 md:space-y-12">
             <Hero />
+            <Comment />
         </div>
     </section>
     )
---
+++ components/page/home/comment.tsx
@@ -0,0 +1,5 @@
+export const Comment = () => {
+   return (
+      <div className="giscus giscus-frame max-w-screen-xl"></div>
+   )
+};
